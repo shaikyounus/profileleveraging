49b6d954548f076f4d5ee55b137f900b21599bdc
SHAIK MOHAMMAD YOUNUS <37431701+shaikyounus@users.noreply.github.com>
2020-12-02 23:21:33 +0530

    Update core.py

--- core.py
+++ core.py
@@ -336,7 +336,7 @@ def res(jobfile,skillset,jd_exp,min_qual):
         #print(Resume_nonTechSkills_vector)
         #print(Resume_exp_vector)
         
-        final_rating = round(similarity*jd_weightage,2)+Resume_skill_vector.__getitem__(index)+Resume_nonTechSkills_vector.__getitem__(index)+Resume_exp_vector.__getitem__(index)
+        final_rating = round(similarity*jd_weightage,2)+Resume_skill_vector.__getitem__(index)+Resume_nonTechSkills_vector.__getitem__(index)
         
         res = ResultElement(round(similarity*jd_weightage,2), os.path.basename(tempList.__getitem__(index)),round(Resume_skill_vector.__getitem__(index),2),
                            Resume_name_vector.__getitem__(index), Resume_phoneNo_vector.__getitem__(index),Resume_email_vector.__getitem__(index),
